871b92a003a6cc246a35b8d88420b2fb9a83d561
Tvejzer <art2q5@gmail.com>
2024-01-27 16:32:00 +0100

    More clouds

---
+++ Assets/Developers/Matvei/GasScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GasScript : MonoBehaviour
+{
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+
+
+}
