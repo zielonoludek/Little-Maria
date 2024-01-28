7473d4ffbf23ecdfd5ce2be41996e1f08bc33f5c
Martyna Cichosz <martynacichosz2@gmail.com>
2024-01-28 12:55:52 +0100

    gas

--- Assets/Scripts/Mechanics/Gas.cs
+++ Assets/Scripts/Mechanics/Gas.cs
@@ -7,7 +7,7 @@ public class Gas : MonoBehaviour
     private Camera camera;
     private Rigidbody2D rb;
     private PlayerController player;
-    private float speed = 200;
+    private float speed = 300;
     [SerializeField] bool isStatic = false;
 
     public void Awake()
@@ -22,7 +22,7 @@ public class Gas : MonoBehaviour
         Vector3 direction = (target - transform.position).normalized;
         if(!isStatic) rb.AddForce(direction * speed);
     }
-    private void OnTriggerExit2D(Collider2D collider)
+    private void OnTriggerStay2D(Collider2D collider)
     {
         if (collider.gameObject.CompareTag("Room")) Destroy(gameObject);
         else if (collider.gameObject.CompareTag("Movable"))
