f47f0f81c94c5ec0ac423524d24dc5629d5d59d4
CM\michal.szczachor <mike.programming265@gmail.com>
2024-01-28 14:34:49 +0100

    updated gas UI

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -10,12 +10,19 @@ public class UIManager : MonoBehaviour
     public static Action OnGameUnpaused;
 
     [SerializeField] private Image imageSlot;
+    [SerializeField] private Sprite gas1;
+    [SerializeField] private Sprite gas2;
+    [SerializeField] private Sprite gas3;
+    [SerializeField] private Sprite gas4;
+
+    private PlayerController playerController;
 
     private bool _isGamePaused;
     
     private void Awake()
     {
         Instance = this;
+        playerController = FindObjectOfType<PlayerController>();
     }
 
     private void Start()
@@ -24,7 +31,33 @@ public class UIManager : MonoBehaviour
         imageSlot.enabled = false;
     }
 
-    public void UpdateItemUIPanel(Sprite itemImage)
+    private void Update()
+    {
+        UpdateItemUI();
+    }
+
+    private void UpdateItemUI()
+    {
+        Debug.Log(playerController.GetGasAmount());
+        if (playerController.GetGasAmount() > 4)
+        {
+            imageSlot.sprite = gas1;
+        }
+        else if (playerController.GetGasAmount() > 2 && playerController.GetGasAmount() <= 4)
+        {
+            imageSlot.sprite = gas2;
+        }
+        else if (playerController.GetGasAmount() > 0 && playerController.GetGasAmount() <= 2)
+        {
+            imageSlot.sprite = gas3;
+        }
+        else if (playerController.GetGasAmount() == 0)
+        {
+            imageSlot.sprite = gas4;
+        }
+    }
+
+    public void PickupItemUIPanel(Sprite itemImage)
     {
         imageSlot.sprite = itemImage;
         imageSlot.enabled = true;
