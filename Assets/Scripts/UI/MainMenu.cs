eb389a9dc37f1c0afe7b73987f838281914a3e02
CM\michal.szczachor <mike.programming265@gmail.com>
2024-01-28 15:40:04 +0100

    main menu for 1000 time

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -5,8 +5,10 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] private Button playButton;
+    [SerializeField] private Button creditsButton;
     [SerializeField] private Button quitButton;
-
+    [SerializeField] private Button backToMainMenuButton;
+    [SerializeField] private GameObject creditsPanel;
     private void Awake()
     {
         playButton.onClick.AddListener(() =>
@@ -14,7 +16,22 @@ public class MainMenu : MonoBehaviour
             SceneManager.LoadScene(1);
         } );
         
+        creditsButton.onClick.AddListener(() =>
+        {
+            creditsPanel.SetActive(true);
+        });
+
+        backToMainMenuButton.onClick.AddListener(() =>
+        {
+            creditsPanel.SetActive(false);
+        });
+        
         quitButton.onClick.AddListener(Application.Quit);
         Time.timeScale = 1f;
+    }
+    
+    private void Start()
+    {
+        creditsPanel.SetActive(false);
     }
 }
