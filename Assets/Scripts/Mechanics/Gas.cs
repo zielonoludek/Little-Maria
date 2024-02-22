cbc451f40bc8f9ada486569bb492c16a1f678bd7
Tvejzer <art2q5@gmail.com>
2024-02-22 15:56:12 +0100

    Gas harming delay

--- Assets/Scripts/Mechanics/Gas.cs
+++ Assets/Scripts/Mechanics/Gas.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Gas : MonoBehaviour
@@ -6,6 +7,7 @@ public class Gas : MonoBehaviour
     private Rigidbody2D rb;
     private float speed = 300;
     [SerializeField] bool isStatic = false;
+    bool isHarmless = true;
 
     public void Awake()
     {
@@ -17,13 +19,21 @@ public class Gas : MonoBehaviour
         Vector3 target = camera.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = (target - transform.position).normalized;
         if(!isStatic) rb.AddForce(direction * speed);
+        StartCoroutine(Harm());
     }
     private void OnTriggerExit2D(Collider2D collider)
     {
-        if (collider.gameObject.CompareTag("Room")) Destroy(gameObject);
+        if (isHarmless) return;
+        else if (collider.gameObject.CompareTag("Room")) Destroy(gameObject);
         else if (collider.gameObject.CompareTag("Movable") && collider.gameObject.layer != 7)
         {
             Destroy(collider.gameObject);
         }
+    }
+
+    IEnumerator Harm()
+    {
+        yield return new WaitForSeconds(1.5f);
+        isHarmless = false;
     }
 }
